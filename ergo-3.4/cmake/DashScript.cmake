0c0535f8d94f99a1898dbcde14bd4a8f4d41ef2b
Tourmaline <nastja.kruchinina@gmail.com>
2015-01-13 13:36:28 +0100

    Added possibility for installation in the cmake files

---
+++ ergo-3.4/cmake/DashScript.cmake
@@ -0,0 +1,47 @@
+set(CTEST_SOURCE_DIRECTORY "$ENV{HOME}/Desktop/PROJECTS/ergo_project/repos/ergo")
+set(CTEST_BINARY_DIRECTORY "$ENV{HOME}/Desktop/PROJECTS/ergo_project/repos/ergo/build")
+
+#set(CTEST_SITE "www.ergoscf.org")
+set(CTEST_BUILD_NAME "linux-gcc-default")
+
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_CONFIGURATION "Debug")
+set(CTEST_BUILD_OPTIONS "")
+
+set(WITH_MEMCHECK TRUE)
+set(WITH_COVERAGE TRUE)
+
+#######################################################################
+
+ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+
+find_program(CTEST_GIT_COMMAND NAMES git)
+find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
+find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
+
+# info : https://wiki.wxwidgets.org/Valgrind_Suppression_File_Howto
+#set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE ${CTEST_SOURCE_DIRECTORY}/tests/valgrind.supp)
+
+if(NOT EXISTS "${CTEST_SOURCE_DIRECTORY}")
+  set(CTEST_CHECKOUT_COMMAND "${CTEST_GIT_COMMAND} clone git@github.com:ergo-research/ergo.git ${CTEST_SOURCE_DIRECTORY}")
+endif()
+
+set(CTEST_UPDATE_COMMAND "${CTEST_GIT_COMMAND}")
+
+set(CTEST_CONFIGURE_COMMAND "${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}")
+set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"-G${CTEST_CMAKE_GENERATOR}\"")
+set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"${CTEST_SOURCE_DIRECTORY}\"")
+
+
+ctest_start("Nightly")
+ctest_update()
+ctest_configure()
+ctest_build()
+ctest_test()
+if (WITH_COVERAGE AND CTEST_COVERAGE_COMMAND)
+  ctest_coverage()
+endif (WITH_COVERAGE AND CTEST_COVERAGE_COMMAND)
+if (WITH_MEMCHECK AND CTEST_MEMORYCHECK_COMMAND)
+  ctest_memcheck()
+endif (WITH_MEMCHECK AND CTEST_MEMORYCHECK_COMMAND)
+ctest_submit()
