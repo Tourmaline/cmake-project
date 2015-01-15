21cf30f3ce533bbb5e0c1f60f4c4c48c6d8dabbd
Tourmaline <nastja.kruchinina@gmail.com>
2015-01-15 09:55:24 +0100

    Changed time for nightly build

--- ergo-3.4/CTestConfig.cmake
+++ ergo-3.4/CTestConfig.cmake
@@ -7,7 +7,7 @@
 ##   INCLUDE(CTest)
 
 set(CTEST_PROJECT_NAME "ergo_cmake")
-set(CTEST_NIGHTLY_START_TIME "11:10:00 EST")
+set(CTEST_NIGHTLY_START_TIME "03:50:00 EST")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
