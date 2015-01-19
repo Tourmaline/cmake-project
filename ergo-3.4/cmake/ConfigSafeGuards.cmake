cc8433a558c97529d5817bff626cfa46604a2b85
Tourmaline <nastja.kruchinina@gmail.com>
2015-01-19 12:44:35 +0100

    Changed error message

--- ergo-3.4/cmake/ConfigSafeGuards.cmake
+++ ergo-3.4/cmake/ConfigSafeGuards.cmake
@@ -1,7 +1,7 @@
 
 # guard against in-source builds
 if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
-    message(FATAL_ERROR "In-source builds not allowed. Please make a new directory (called a build directory) and run CMake from there.")
+    message(FATAL_ERROR "In-source builds are not allowed. Please make a new directory (called a build directory) and run CMake from there. In case of error, try to delete generated cmake files from current directory.")
 endif()
 
 
